2600b7d66d9121da08bc69f1ba74bd2b6d212607
kaua-victor <kaua.vp.gomes@gmail.com>
2026-01-22 21:05:58 -0300

    atualizações etapa 4

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -18,16 +18,6 @@ type Adapter struct {
 	payment payment.PaymentClient
 }
 
-//	func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
-//		var opts []grpc.DialOption
-//		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-//		conn, err := grpc.Dial(paymentServiceUrl, opts...)
-//		if err != nil {
-//			return nil, err
-//		}
-//		client := payment.NewPaymentClient(conn)
-//		return &Adapter{payment: client}, nil
-//	}
 func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
 
@@ -48,15 +38,6 @@ func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	client := payment.NewPaymentClient(conn)
 	return &Adapter{payment: client}, nil
 }
-
-// func (a *Adapter) Charge(order *domain.Order) error {
-// 	_, err := a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
-// 		UserId:     order.CustomerID,
-// 		OrderId:    order.ID,
-// 		TotalPrice: order.TotalPrice(),
-// 	})
-// 	return err
-// }
 
 func (a *Adapter) Charge(order *domain.Order) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
